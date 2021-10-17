12bd40c525e16d47942eae54f4f0a361b51198c9
NL <Lapina.n.b@mail.ru>
2021-10-18 00:58:36 +1000

    class дз1

---
+++ Class.js
@@ -0,0 +1,22 @@
+
+class Rabotnic {
+        constructor(name, surname, rate, days){
+this.name=name;
+this.surname=surname;
+this.rate=rate;
+this.days=days;
+    }
+
+    getSalary(){
+    console.log(this.rate * this.days)
+    } 
+}   
+    let rabotnic = new Rabotnic('Катерина', 'Петровна', 10, 31);
+    
+    
+    console.log(rabotnic.name); //выведет 'Катерина'
+    console.log(rabotnic.surname); //выведет 'Петрова'
+    console.log(rabotnic.rate); //выведет 10
+    console.log(rabotnic.days); //выведет 31
+    console.log(rabotnic.getSalary()); //выведет 310 - то есть 10*31
+
